f2aebfe341b8c77020543a0e325fc70407b402f9
Jacob Janak <jakeathotmail@gmail.com>
2022-05-08 00:09:46 -0400

    Adding the two sell functions

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-// Enable all tooltips.
-$(document).ready(function() {
-	$('[data-toggle="tooltip"]').tooltip();
-});
 
 // User Ethereum wallet public address.
 let walletAddress = '';
 
 // Smart contract.
 const contract = {
-	chainId: '0x4', // Kovan test network.
+	chainId: '0x4', // Rinkeby test network.
 	address: '0xEcb6f4CE53a36943B801659c9719d84eca970eD6', // Contract public address
-	// writeAddress: '0xfcc74f71', // Hex address of the write method of the contract.
-	// storage: { text: null }, // Contract storage contains only bytes32 public text.
 
 	buyBull: async function(amount, callback) {
 		const methodAddress = '0x349a363f';
@@ -39,7 +33,47 @@ const contract = {
 			}],
 		});
 		if (callback) callback(txHash);
-	}
+	},
+
+	sellBull: async function(numerator, denominator, callback) {
+		const methodAddress = '0x594da77f';
+
+		// convert inputs to be 64 digit hexadecimal strings
+		numerator = numerator.toString(16);
+		denominator = denominator.toString(16);
+		while (numerator.length < 64) numerator = '0' + numerator;
+		while (denominator.length < 64) denominator = '0' + denominator;
+
+		const txHash = await ethereum.request({
+		  	method: 'eth_sendTransaction',
+		  	params: [{
+			  	to: this.address,
+			  	from: walletAddress,
+			  	data: methodAddress + numerator + denominator,
+			}],
+		});
+		if (callback) callback(txHash);
+	},
+
+	sellBear: async function(numerator, denominator, callback) {
+		const methodAddress = '0xdde1aa11';
+
+		// convert inputs to be 64 digit hexadecimal strings
+		numerator = numerator.toString(16);
+		denominator = denominator.toString(16);
+		while (numerator.length < 64) numerator = '0' + numerator;
+		while (denominator.length < 64) denominator = '0' + denominator;
+
+		const txHash = await ethereum.request({
+		  	method: 'eth_sendTransaction',
+		  	params: [{
+			  	to: this.address,
+			  	from: walletAddress,
+			  	data: methodAddress + numerator + denominator,
+			}],
+		});
+		if (callback) callback(txHash);
+	},
 };
 
 // Check if user has MetaMask installed.
@@ -49,9 +83,7 @@ function hasMetamask () {
 
 // Prompt user to enable MetaMask through the extension.
 async function enableMetamask(callback) {
-	const accounts = await ethereum.request({
-		method: 'eth_requestAccounts'
-	});
+	const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 	walletAddress = accounts[0];
 	
 	// Update DOM.
@@ -72,7 +104,7 @@ async function switchChain(chainId, callback) {
 // Button to prompt user to enable metamask.
 $('#connect-wallet').on('click', function() { 
 	if (!hasMetamask()) {
-	  	window.alert("No wallet detected. Please install MetaMask or another Ethereum wallet.");
+	  	window.alert("No Ethereum wallet detected. Please install MetaMask.");
 	} else {
 		enableMetamask(function() {
 			if (ethereum.chainId != contract.chainId) {
@@ -87,4 +119,9 @@ $('#connect-wallet').on('click', function() {
 			}
 		});
 	}
+});
+
+// Enable all tooltips.
+$(document).ready(function() {
+	$('[data-toggle="tooltip"]').tooltip();
 });
